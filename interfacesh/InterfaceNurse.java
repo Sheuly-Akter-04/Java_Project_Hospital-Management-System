516383658a1a5aaf5718dc07313f8fb22548cd7b
Sheuly Akter <77951733+Sheuly-Akter-04@users.noreply.github.com>
2022-11-12 03:09:11 +0600

    Add files via upload

---
+++ interfacesh/InterfaceNurse.java
@@ -0,0 +1,11 @@
+package interfacesh;
+import java.lang.*;
+public interface InterfaceNurse
+{
+    public static void disp()
+    {
+        System.out.println("---Inside Nurse Interface---");
+    }
+    public void showInfo();
+   
+}
---
+++ interfacesh/InterfacePatient.java
@@ -0,0 +1,11 @@
+package interfacesh;
+import java.lang.*;
+public interface InterfacePatient
+{
+    public static void disp()
+    {
+        System.out.println("---Inside Interface---");
+    }
+    public void displayinfo();
+   
+}
